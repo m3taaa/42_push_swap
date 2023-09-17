e5bd29b2ba8da52bd683be23e9fa6d956488fde8
Mathieu <xyz@Xyz.local>
2023-09-18 00:00:10 +0200

    Update

--- src/parsing.c
+++ src/parsing.c
@@ -6,27 +6,13 @@
 /*   By: mmeerber <mmeerber@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 15:23:28 by mmeerber          #+#    #+#             */
-/*   Updated: 2023/09/17 15:44:48 by mmeerber         ###   ########.fr       */
+/*   Updated: 2023/09/17 23:54:49 by mmeerber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/*int verif_number(char c)
-{
-	if ()
-}*/
-
 void	parsing(char *string)
 {
-	int x;
-
-	x = 0;
-	printf("len string %zu\n", ft_strlen(string));
-	printf("String : \n%s", string);
-	while (string[x])
-	{
-
-		x++;
-	}
+	printf("%s\n", string);
 }
